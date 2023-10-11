1cec90fdd711c1e1ace331564f7b3318f0124b13
DrPandaMonium <105460824+DrPandaMonium@users.noreply.github.com>
2023-10-11 17:00:20 -0500

    Quote machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const App = () => {
+
+    const [quoteInfo, setQuoteInfo] = useState({});
+
+    useEffect(() => {
+        getQuote();
+    }, [])
+
+    const getQuote = () => {
+        fetch('https://api.quotable.io/random')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setQuoteInfo({
+                    text: data.content,
+                    author: data.author
+                })
+                console.log(data)
+            });
+    }
+
+    return (
+        <div className='app'>
+            <div id='quote-box'>
+                <h1 id='text'>{quoteInfo.text}</h1>
+                <h2 id='author'>-{quoteInfo.author}</h2>
+                <button id='new-quote' onClick={getQuote}>New Quote</button>
+                <a id='tweet-quote' href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + quoteInfo.text}target='_blank'>Tweet</a>
+            </div>
+        </div>
+    )
 }
 
 export default App;
